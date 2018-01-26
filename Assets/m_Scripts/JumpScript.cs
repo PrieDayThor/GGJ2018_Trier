f4f3b6a875fb7bae0954cde8b8feaf1ebbe2c875
WireDepp <sparkle@pp-1x-134-170.wlan-ng.fh-trier.de>
2018-01-26 22:25:40 +0100

    First Jumping Cubes

---
+++ Assets/m_Scripts/JumpScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+
+
+    public void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("Test");
+            rb.AddForce(Vector3.up * 100.0f);
+        }
+    }
+}
---
+++ Assets/m_Scripts/NetworkSetup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(NetworkManager))]
+public class NetworkSetup : MonoBehaviour {
+
+    private NetworkManager manager;
+
+	// Use this for initialization
+	void Start () {
+        manager = GetComponent<NetworkManager>();
+	}
+
+    public void SetIP(string ip){
+        manager.networkAddress = ip;
+    }
+
+    public void SetPort(int port){
+        manager.networkPort = port;
+    }
+
+    public string GetIP(){
+        return Network.player.ipAddress;
+    }
+
+    public void PrintIP() {
+        Debug.Log(Network.player.ipAddress);
+    }
+
+    public void StartHost(){
+        manager.StartHost();
+    }
+
+    public void Join() {
+        manager.StartClient();
+    }
+
+}
