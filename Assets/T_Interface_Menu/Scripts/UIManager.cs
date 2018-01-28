86e7a2c47a88d407cc1d0915d1f8cbd0ef169a8d
Tancat1 <tanjaferencik@web.de>
2018-01-28 10:09:34 +0100

    Noch bessere Doku
    slider drin, andere szene raus

---
+++ Assets/T_Interface_Menu/Scripts/UIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+
+    public void disableBoolInAnimator(Animator anim)
+    {
+        anim.SetBool("isDisplayed", false);
+    }
+
+    public void enableBoolInAnimator(Animator anim)
+    {
+        anim.SetBool("isDisplayed", true);
+    }
+
+    public void disableMainInAnimator(Animator anim)
+    {
+        anim.SetBool("mainMenu", false);
+    }
+
+    public void enableMainInAnimator(Animator anim)
+    {
+        anim.SetBool("mainMenu", true);
+    }
+
+    public void disableCreditsInAnimator(Animator anim)
+    {
+        anim.SetBool("creditsMenu", false);
+    }
+
+    public void enableCreditsInAnimator(Animator anim)
+    {
+        anim.SetBool("creditsMenu", true);
+    }
+
+    public void disableSettingslInAnimator(Animator anim)
+    {
+        anim.SetBool("settingsMenu", false);
+    }
+
+    public void enableSettingsInAnimator(Animator anim)
+    {
+        anim.SetBool("settingsMenu", true);
+    }
+
+    public void PRINT()
+    {
+        print("PRITNIEONT");
+    }
+}
